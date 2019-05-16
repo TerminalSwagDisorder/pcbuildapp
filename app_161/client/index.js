4036b0e2c859b9bed3bb9e849d57509d80664056
TerminalSwagDisorder <benniw139@gmail.com>
2019-05-16 14:04:48 +0300

    removed old app_161 and pcbuildapp

--- app_161/client/index.js
+++
@@ -1,8 +0,0 @@
-/*
-'http://localhost:8001/pcbuildapp-1170c5b/case.json', 'http://localhost:8001/pcbuildapp-1170c5b/cpu.json', 'http://localhost:8001/pcbuildapp-1170c5b/cpu-cooler.json', 'http://localhost:8001/pcbuildapp-1170c5b/fan.json', 'http://localhost:8001/pcbuildapp-1170c5b/gpu.json', 'http://localhost:8001/pcbuildapp-1170c5b/memory.json', 'http://localhost:8001/pcbuildapp-1170c5b/motherboard.json', 'http://localhost:8001/pcbuildapp-1170c5b/psu.json', 'http://localhost:8001/pcbuildapp-1170c5b/storage.json', 'http://localhost:8001/pcbuildapp-1170c5b/wired.json', 'http://localhost:8001/pcbuildapp-1170c5b/wireless.json'
-*/
-/*
-//select * from cpu where "name" like '%AMD%'
-http://localhost:8001/pcbuildapp-1170c5b?sql=select+*+from+cpu+where+%22name%22+like+%27%25AMD%25%27
-*/
-
